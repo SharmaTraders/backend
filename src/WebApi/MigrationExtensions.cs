f73a605acfa0c899da02d260ae16eba5b18de73f
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2024-03-10 16:50:31 +0100

    BP-37 : Use local container for development

---
+++ src/WebApi/MigrationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi;
+
+public static class MigrationExtensions {
+    public static void ApplyMigrations(this IApplicationBuilder app) {
+        using var serviceScope = app.ApplicationServices.CreateScope();
+        using DbContext context = serviceScope.ServiceProvider.GetRequiredService<DbContext>();
+        context.Database.Migrate();
+    }
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -86,6 +87,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
 
 public partial class Program {
 
