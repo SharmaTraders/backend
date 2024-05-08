841615c87e522c4d26c63b3b1d6c43889dd222b1
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2024-05-08 17:40:10 +0200

    Simple bug fix with database

--- src/Data/Repository/BillingPartyRepository.cs
+++ src/Data/Repository/BillingPartyRepository.cs
@@ -22,16 +22,17 @@ public class BillingPartyRepository : IBillingPartyRepository {
 
 
     public async Task<bool> IsUniqueNameAsync(string name, [Optional] Guid idToExclude) {
-        bool doesNameExist = await _context.BillingParties.AnyAsync(bp => bp.Name.ToLower().Equals(name.ToLower())
-                                                                          && bp.Id != idToExclude);
+        bool doesNameExist = await _context.BillingParties.AnyAsync(
+            bp => bp.Id != idToExclude && bp.Name.ToLower().Equals(name.ToLower())
+        );
         return !doesNameExist;
     }
 
     public async Task<bool> IsUniqueVatNumberAsync(string vatNumber, [Optional] Guid idToExclude) {
         bool doesVatNumberExist = await _context.BillingParties
             .AnyAsync(party =>
-                !string.IsNullOrEmpty(party.VatNumber) && party.VatNumber.ToLower().Equals(vatNumber.ToLower())
-                && party.Id != idToExclude);
+                !string.IsNullOrEmpty(party.VatNumber) && party.Id != idToExclude && party.VatNumber.ToLower().Equals(vatNumber.ToLower())
+                );
 
         return !doesVatNumberExist;
     }
@@ -43,8 +44,8 @@ public class BillingPartyRepository : IBillingPartyRepository {
     public async Task<bool> IsUniqueEmailAsync(string email, [Optional] Guid idToExclude) {
         bool doesEmailExist = await _context.BillingParties
             .AnyAsync(party =>
-                !string.IsNullOrEmpty(party.Email) && party.Email.ToLower().Equals(email.ToLower()) &&
-                party.Id != idToExclude);
+                !string.IsNullOrEmpty(party.Email) && party.Id != idToExclude &&party.Email.ToLower().Equals(email.ToLower())
+                );
         return !doesEmailExist;
     }
 }
--- src/Domain/billingParty/BillingPartyDomain.cs
+++ src/Domain/billingParty/BillingPartyDomain.cs
@@ -19,26 +19,20 @@ public class BillingPartyDomain : IBillingPartyDomain {
 
     public async Task CreateBillingParty(CreateBillingPartyRequest request) {
         BillingPartyEntity partyEntity = BillingPartyConverter.ToEntity(request);
-        List<Task> tasks = [
-            CheckForUniqueName(request.Name),
-            CheckForUniqueVatNumber(request.VatNumber!),
-            CheckForUniqueEmail(request.Email!)
-        ];
-
-        // Wait for all tasks to complete
-        await Task.WhenAll(tasks);       
+        await CheckForUniqueName(request.Name);
+        await CheckForUniqueVatNumber(request.VatNumber!);
+        await CheckForUniqueEmail(request.Email!);
 
         await _billingPartyRepository.AddAsync(partyEntity);
         await _unitOfWork.SaveChangesAsync();
     }
 
     public async Task<ICollection<BillingPartyDto>> GetAllBillingParties() {
-        List<BillingPartyEntity>  billingPartyEntities = await _billingPartyRepository.GetAllAsync();
+        List<BillingPartyEntity> billingPartyEntities = await _billingPartyRepository.GetAllAsync();
         return BillingPartyConverter.ToDtoList(billingPartyEntities);
     }
 
     public async Task UpdateBillingParty(string id, UpdateBillingPartyRequest request) {
-
         bool tryParse = Guid.TryParse(id, out Guid guid);
         if (!tryParse) {
             throw new DomainValidationException("Id", ErrorCode.BadRequest, ErrorMessages.IdInvalid);
@@ -49,14 +43,10 @@ public class BillingPartyDomain : IBillingPartyDomain {
             throw new DomainValidationException("Id", ErrorCode.NotFound, ErrorMessages.BillingPartyNotFound(guid));
         }
 
-        List<Task> tasks = [
-            CheckForUniqueName(request.Name, guid),
-            CheckForUniqueVatNumber(request.VatNumber!, guid),
-            CheckForUniqueEmail(request.Email!, guid)
-        ];
-
-        // Wait for all tasks to complete
-        await Task.WhenAll(tasks);
+        // I tried running the following in parallel but it was causing issues with the database
+        await CheckForUniqueName(request.Name, guid);
+        await CheckForUniqueVatNumber(request.VatNumber!, guid);
+        await CheckForUniqueEmail(request.Email!, guid);
 
         billingPartyEntity.PhoneNumber = request.PhoneNumber;
         billingPartyEntity.Email = request.Email;
@@ -69,33 +59,24 @@ public class BillingPartyDomain : IBillingPartyDomain {
     private async Task CheckForUniqueEmail(string requestEmail, [Optional] Guid idToExclude) {
         bool isUniqueEmail = await _billingPartyRepository.IsUniqueEmailAsync(requestEmail, idToExclude);
         if (!isUniqueEmail) {
-            throw new DomainValidationException("Email", ErrorCode.Conflict, ErrorMessages.BillingPartyEmailAlreadyExists(requestEmail));
+            throw new DomainValidationException("Email", ErrorCode.Conflict,
+                ErrorMessages.BillingPartyEmailAlreadyExists(requestEmail));
         }
-        
     }
 
     private async Task CheckForUniqueVatNumber(string requestVatNumber, [Optional] Guid idToExclude) {
         bool isUniqueVatNumber = await _billingPartyRepository.IsUniqueVatNumberAsync(requestVatNumber, idToExclude);
         if (!isUniqueVatNumber) {
-            throw new DomainValidationException("VatNumber", ErrorCode.Conflict, ErrorMessages.BillingPartyVatNumberAlreadyExists(requestVatNumber));
+            throw new DomainValidationException("VatNumber", ErrorCode.Conflict,
+                ErrorMessages.BillingPartyVatNumberAlreadyExists(requestVatNumber));
         }
     }
 
     private async Task CheckForUniqueName(string requestName, [Optional] Guid idToExclude) {
         bool isUniqueName = await _billingPartyRepository.IsUniqueNameAsync(requestName, idToExclude);
         if (!isUniqueName) {
-            throw new DomainValidationException("Name", ErrorCode.Conflict, ErrorMessages.BillingPartyNameAlreadyExists(requestName));
+            throw new DomainValidationException("Name", ErrorCode.Conflict,
+                ErrorMessages.BillingPartyNameAlreadyExists(requestName));
         }
     }
-
- 
-
-   
-
-
-
-
-  
-
-    
 }
