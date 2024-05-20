da12d1aefd56e0838dc8174ba70feb3461b7120a
Himal sharma <41054866+himal28924@users.noreply.github.com>
2024-05-20 12:33:36 +0200

    GetInvoice (#21)
    * Can get purchase and sale

---
+++ src/Query/sale/GetAllSaleHandler.cs
@@ -0,0 +1,58 @@
+﻿using Domain.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using QueryContracts.sale;
+
+namespace Query.sale
+{
+    public class GetAllSaleHandler : IRequestHandler<GetAllSales.Query, GetAllSales.Answer>
+    {
+        private readonly SharmaTradersContext _context;
+
+        public GetAllSaleHandler(SharmaTradersContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetAllSales.Answer> Handle(GetAllSales.Query request, CancellationToken cancellationToken)
+        {
+            var queryable = _context.Sales
+                .Include(sale => sale.SaleLineItems)
+                .Include(sale => sale.BillingParty)
+                .OrderByDescending(sale => sale.Date)
+                .AsQueryable();
+
+            int totalCount = await queryable.CountAsync(cancellationToken);
+
+            List<GetAllSales.SaleDto> saleDtos = await queryable
+                .Skip((request.PageNumber - 1) * request.PageSize)
+                .Take(request.PageSize)
+                .Select(sale => new GetAllSales.SaleDto(
+                    sale.Id.ToString(),
+                    sale.BillingParty.Name,
+                    sale.InvoiceNumber,
+                    sale.Date.ToString(),
+                    GetTotalAmount(sale.SaleLineItems, sale.TransportFee, sale.VatAmount),
+                    GetRemainingAmount(sale.SaleLineItems, sale.TransportFee, sale.VatAmount, sale.ReceivedAmount),
+                    sale.Remarks
+                ))
+                .ToListAsync(cancellationToken);
+
+            return new GetAllSales.Answer(saleDtos, totalCount, request.PageNumber, request.PageSize);
+        }
+
+        private static double GetTotalAmount(ICollection<SaleLineItem> saleLineItems, double? transportFee, double? vatAmount)
+        {
+            double totalAmount = saleLineItems.Sum(item => ((item.Price * item.Quantity) - (item.Report ?? 0)));
+            totalAmount += transportFee ?? 0;
+            totalAmount += vatAmount ?? 0;
+            return Math.Round(totalAmount, 2);
+        }
+
+        private static double GetRemainingAmount(ICollection<SaleLineItem> saleLineItems, double? transportFee, double? vatAmount, double? receivedAmount)
+        {
+            double totalAmount = GetTotalAmount(saleLineItems, transportFee, vatAmount);
+            return totalAmount - (receivedAmount ?? 0);
+        }
+    }
+}
---
+++ src/QueryContracts/purchase/GetAllPurchases.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace QueryContracts.purchase;
+
+public static class GetAllPurchases
+{
+    public record Query(int PageNumber, int PageSize) : IRequest<Answer>;
+    
+    public record Answer(ICollection<PurchaseDto> Purchases, int TotalCount, int PageNumber, int PageSize);
+    
+    public record PurchaseDto(string Id, string PartyName , int? InvoiceNo , string Date, double TotalAmount, double? RemainingBalance, string? Remarks);
+}
